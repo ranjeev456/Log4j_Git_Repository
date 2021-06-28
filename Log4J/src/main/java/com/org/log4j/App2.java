a1073250cb3b9ee77d65479229d8a26a2d48036f
ranjeev <ranjeevkumar456@gmail.com>
2021-06-28 10:45:04 +0530

    this is my first commit by software engineer

---
+++ Log4J/src/main/java/com/org/log4j/App2.java
@@ -0,0 +1,17 @@
+package com.org.log4j;
+
+import org.apache.log4j.Logger;
+
+public class App2 
+{
+	
+	static Logger log=Logger.getLogger(App2.class.getName());
+    public static void main( String[] args )
+    {
+    	System.out.println( "Hello World!" );
+    	log.debug("Hello this is a debug message");
+    	log.info("Hello this is an info message");
+    	log.info("Hello this ia an info mesage from ranjeev");
+    	log.info("Hello this changed has been done by software engineer");
+    }
+}
