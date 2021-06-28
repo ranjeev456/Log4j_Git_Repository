2c73ed2ae0748caf97e3eac5553d4cb42f0cd6c7
ranjeev <ranjeevkumar456@gmail.com>
2021-06-28 09:24:36 +0530

    First commit

---
+++ Log4J/src/main/java/com/org/log4j/App.java
@@ -0,0 +1,15 @@
+package com.org.log4j;
+
+import org.apache.log4j.Logger;
+
+public class App 
+{
+	
+	static Logger log=Logger.getLogger(App.class.getName());
+    public static void main( String[] args )
+    {
+    	System.out.println( "Hello World!" );
+    	log.debug("Hello this is a debug message");
+    	log.info("Hello this is an info message");
+    }
+}
